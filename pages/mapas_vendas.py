c0098fed752249c69ef0a9586e0c92e97f43eb22
Napoleão Barbosa da cunha Júnior <juniorcunhabarbosa@gmail.com>
2026-03-20 19:37:54 -0300

    atualizacao

---
+++ pages/mapas_vendas.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title('Mapas de vendas por localização')
+st.header('localização das vendas')
+
+col1,col2,col3,col4 = st.columns(4)
--- pages/sobre.py
+++ pages/sobre.py
@@ -4,13 +4,11 @@ st.title('ℹ️ Sobre o Dashboard')
 st.header("""Dashboard de Análise de Vendas
 Este dashboard foi desenvolvido como
            projeto integrador do curso de Streamlit.""")
-st.markdown("""Tecnologias utilizadas:
-Streamlit
-— Framework para apps web em Python Pandas 
-— Manipulação e análise de dados
-Plotly Express —
- Visualizações interativas
-NumPy — Geração de dados numéricos""")
+st.markdown("Tecnologias utilizadas:")
+st.markdown("Streamlit — Framework para apps web em Python")
+st.markdown("Pandas — Manipulação e análise de dados")
+st.markdown("Plotly Express — Visualizações interativas")
+st.markdown("NumPy — Geração de dados numéricos")
 st.markdown('Funcionalidades:')
 st.markdown('📊 Visão geral com KPIs e gráficos resumo')
 st.markdown('💰 Análise detalhada de vendas com filtros')
